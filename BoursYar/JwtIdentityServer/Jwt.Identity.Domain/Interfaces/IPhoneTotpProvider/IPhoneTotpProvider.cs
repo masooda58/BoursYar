d5d747a73c294b51c207ba95e259fb5650a5b36f
masoud.a58@gmail.com <masoud.a58@gmail.com>
2022-08-24 18:39:34 +0430

    add Totp interface Provider

---
+++ BoursYar/JwtIdentityServer/Jwt.Identity.Domain/Interfaces/IPhoneTotpProvider/IPhoneTotpProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace Jwt.Identity.Domain.Interfaces.IPhoneTotpProvider
+{
+    public interface IPhoneTotpProvider
+    {
+        public string GenerateTotp(string secretKey);
+        public bool VerifyTotp(string secretKey, string code);
+    }
+}
