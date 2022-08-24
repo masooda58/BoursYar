41f3a8cb46fae72cec9000624971117b9b9369df
masoud.a58@gmail.com <masoud.a58@gmail.com>
2022-08-24 18:39:06 +0430

    Email Sender Interface

---
+++ BoursYar/JwtIdentityServer/Jwt.Identity.Domain/Interfaces/IEmailSender/IEmailSender.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Jwt.Identity.Domain.Interfaces.IEmailSender
+{
+    public interface IEmailSender
+    {
+       
+        public Task SendEmailAsync(string toEmail,string subject, string message,bool isMessageHtml=false);
+    }
+}
