71a7394a6cc2024858eb99c39beb9d3cd74e33c2
masoud.a58@gmail.com <masoud.a58@gmail.com>
2022-08-28 03:40:15 +0430

    Change mobileNo Regx

--- BoursYar/JwtIdentityServer/PersianTranslation/DataAnnotations/MobileNoAttribute.cs
+++ BoursYar/JwtIdentityServer/PersianTranslation/DataAnnotations/MobileNoAttribute.cs
@@ -9,7 +9,8 @@ namespace PersianTranslation.DataAnnotations
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
             string valueAsString = value.ToString();
-            const string mobPattern = @"^((\+9|\+989|\+\+989|9|09|989|0989|00989)(01|02|03|10|11|12|13|14|15|16|17|18|19|20|21|22|30|31|32|33|34|35|36|37|38|39|90))(\d{7})$";
+           // const string mobPattern = @"^((\+989|9|09|989|00989)(01|02|03|04|05|10|11|12|13|14|15|16|17|18|19|20|21|22|30|31|32|33|34|35|36|37|38|39|90|91|92|93|94))(\d{7})$";
+           const string mobPattern = @"^(9|09|00989|989|\+989)(\d{9})$";
             bool isValidPhone = Regex.IsMatch(valueAsString, mobPattern);
 
             if (isValidPhone)
--- BoursYar/JwtIdentityServer/PersianTranslation/DataAnnotations/PhoneOrEmailAttribute.cs
+++ BoursYar/JwtIdentityServer/PersianTranslation/DataAnnotations/PhoneOrEmailAttribute.cs
@@ -12,24 +12,18 @@ namespace PersianTranslation.DataAnnotations
         {
             string valueAsString = value.ToString();
 
-            const string emailRegex = @"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$";
-            bool isValidEmail = Regex.IsMatch(valueAsString, emailRegex);
+          
+           var emailValidtionAttribute = new EmailAddressAttribute();
+           var mobilValidation = new MobileNoAttribute();
 
-            if (isValidEmail)
+            if (emailValidtionAttribute.IsValid(valueAsString)||mobilValidation.IsValid(valueAsString))
             {
                 return ValidationResult.Success;
             }
 
-            // const string usaPhoneNumbersRegex = @"\(?\d{3}\)?-? *\d{3}-? *-?\d{4}";
-            const string mobPattern = @"^((\+9|\+989|\+\+989|9|09|989|0989|00989)(01|02|03|10|11|12|13|14|15|16|17|18|19|20|21|22|30|31|32|33|34|35|36|37|38|39|90))(\d{7})$";
-            bool isValidPhone = Regex.IsMatch(valueAsString, mobPattern);
-
-            if (isValidPhone)
-            {
-                return ValidationResult.Success;
-            }
-
-            return new ValidationResult("ایمیل یا شماره موبایل معتبر نیست");
+            return valueAsString.Contains("@") ?
+                new ValidationResult("ایمیل وارد شده معتبر نیست") 
+                : new ValidationResult("شماره موبایل وارد شده معتبر نیست");
         }
     }
 }
