71e5a985feebd452fef7eef96383894ff49bc60f
masoud.a58@gmail.com <masoud.a58@gmail.com>
2022-09-02 00:26:34 +0430

    Change name to persian Helper

---
+++ BoursYar/JwtIdentityServer/IdentityPersianHelper/DataAnnotations/PhoneOrEmailAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityPersianHelper.DataAnnotations
+{
+    public class PhoneOrEmailAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                string valueAsString = value.ToString();
+
+
+                var emailValidtionAttribute = new EmailAddressAttribute();
+                var mobilValidation = new MobileNoAttribute();
+
+                if (emailValidtionAttribute.IsValid(valueAsString) || mobilValidation.IsValid(valueAsString))
+                {
+                    return ValidationResult.Success;
+                }
+
+                return valueAsString.Contains("@")
+                    ? new ValidationResult("ایمیل وارد شده معتبر نیست")
+                    : new ValidationResult("شماره موبایل وارد شده معتبر نیست");
+
+
+            }
+
+            return ValidationResult.Success;
+
+        }
+    }
+}
