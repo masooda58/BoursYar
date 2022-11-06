5385a6f54627d8d6158ba3a4690d9ec6ea59a89c
masoud.a58@gmail.com <masoud.a58@gmail.com>
2022-11-06 20:46:21 +0330

    add unit of work

---
+++ BoursYar/JwtIdentityServer/Jwt.Identity.Data/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Jwt.Identity.Data.Context;
+using Jwt.Identity.Data.IntialData;
+using Jwt.Identity.Data.Repositories.ClientRepository;
+using Jwt.Identity.Domain.Clients.Data;
+
+namespace Jwt.Identity.Data.UnitOfWork
+{
+    public class UnitOfWork
+    {
+        private readonly IdentityContext _context;
+        private ClientRepositoryService _client;
+
+        public UnitOfWork(IdentityContext context)
+        {
+            _context = context;
+        }
+
+        public ClientRepositoryService ClientRepository
+        {
+            get
+            {
+                if (_client == null)
+                {
+                    _client = new ClientRepositoryService(_context);
+                }
+
+                return _client;
+            }
+        }
+
+        public void savechange()
+        {
+            _context.SaveChangesAsync();
+        }
+    }
+}
