785f42e247a43836d7bf445c429bd51068ee5993
masoud.a58@gmail.com <masoud.a58@gmail.com>
2022-11-06 20:44:32 +0330

    client interface repository

--- BoursYar/JwtIdentityServer/Jwt.Identity.Domain/Clients/Data/IClientRepository.cs
+++ BoursYar/JwtIdentityServer/Jwt.Identity.Domain/Clients/Data/IClientRepository.cs
@@ -1,9 +1,13 @@
-﻿using Jwt.Identity.Domain.Clients.Entity;
+﻿using System.Threading.Tasks;
+using Jwt.Identity.Domain.Clients.Entity;
 using Jwt.Identity.Domain.IServices;
+using Jwt.Identity.Framework.Response;
 
 namespace Jwt.Identity.Domain.Clients.Data
 {
     public interface IClientRepository : IBaseRepositoryService<Client>
     {
+        public Task<ResultResponse> GetByClientNameAsync(string clientName);
+        public Task<ResultResponse> DeleteClientNameAsync(string clientName);
     }
 }
